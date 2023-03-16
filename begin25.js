0555bee5adc0902ef6717bc4e8d7c6afb1521732
TaniKroos <99119747+TaniKroos@users.noreply.github.com>
2023-03-16 15:22:45 +0530

    Add files via upload

---
+++ begin25.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=aGbWI6r8d3w");
+    });
+
+    document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=ySa7Km4kOCw");
+    });
+
+    document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=tQhrwkmP0TM");
+    });
+
+    document.getElementsByClassName("fa-brands fa-youtube")[3].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=Dpjd80PhBBk");
+    });
+
+    document.getElementsByClassName("fa-brands fa-youtube")[4].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=1gf5RjtwjkA");
+    });
+    document.getElementsByClassName("fa-brands fa-youtube")[5].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=HHHVuyfDp8U");
+    });
+
+   
+}
